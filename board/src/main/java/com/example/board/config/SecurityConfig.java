627dbe411add6a3eeadfba314b1ca2f3b50b235c
GGoReb <seorab@naver.com>
2022-03-24 17:48:56 +0900

    security login 화면 관련

---
+++ board/src/main/java/com/example/board/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.board.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    super.configure(web);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.formLogin().loginPage("/login");
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    super.configure(auth);
+  }
+}

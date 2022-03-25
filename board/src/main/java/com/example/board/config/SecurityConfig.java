46cc1f498b2302a28f72dae8a4502a55155d6739
GGoReb <seorab@naver.com>
2022-03-25 09:00:06 +0900

    권한 관련

--- board/src/main/java/com/example/board/config/SecurityConfig.java
+++ board/src/main/java/com/example/board/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/**").permitAll();
     http.formLogin().loginPage("/login");
   }
 
